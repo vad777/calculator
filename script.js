436a40f6fe6d52057b51f8d1c7228613896e564b
vad777 <69648486+vad777@users.noreply.github.com>
2023-03-03 18:09:27 +0200

    ramgeslider

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 (function ($) {
-  $(function () {
-    
+    $(function () {
+
         $("input[type=range]").rangeslider({polyfill: false});
 
-        var c = $("#stp-clients-attracted"),
+        var
+            c = $("#stp-clients-attracted"),
             d = $("#stp-daily-orders-per-client");
 
-      $(".js-calculator_input-wrap > .js-calculator_text-input").on("change input", function () {
-        $(this).parent().find(".js-calculator_range").val($(this).val()).change();
+        $(".js-calculator_input-wrap > .js-calculator_text-input").on("change input", function () {
+            $(this).parent().find(".js-calculator_range").val($(this).val()).change();
 
-      });
-
-      $(".js-calculator_input-wrap > .js-calculator_range").on("change input", function () {
-        $(this).parent().find(".js-calculator_text-input").val($(this).val());
-      });
-
-      $.fn.digits = function () {
-        return this.each(function () {
-          $(this).text($(this).text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1'"));
         });
-      };
 
-  });
+        $(".js-calculator_input-wrap > .js-calculator_range").on("change input", function () {
+            $(this).parent().find(".js-calculator_text-input").val($(this).val());
+        });
+
+        $.fn.digits = function () {
+            return this.each(function () {
+                $(this).text($(this).text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1'"));
+            });
+        };
+
+    });
 })(jQuery);
